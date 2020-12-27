2ce7897179d6cf711837d019c8a55f3ad5333627
byfarux25 <76660037+byfarux25@users.noreply.github.com>
2020-12-28 00:35:02 +0300

    Add files via upload

--- Front-End/Request.js
+++ Front-End/Request.js
@@ -46,31 +46,5 @@ class Request {
 
 
 
-// request.get("https://jsonplaceholder.typicode.com/albums")
-// .then(albums => {
-    
-//     console.log(albums);
-// })
-// .catch(err => console.log(err));
-
-// request.post("https://jsonplaceholder.typicode.com/albums",{userId:1,title:"Thriller"})
-// .then(newAlbum => console.log(newAlbum))
-// .catch(err => console.log(err));
-
-
-// request.put("https://jsonplaceholder.typicode.com/albums/10",{userId:10,title:"Tarkan Karma"})
-// .then(album => console.log(album))
-// .catch(err => console.log(err));
-
-
-
-// request.delete("https://jsonplaceholder.typicode.com/albums/1")
-// .then(message => console.log(message) )
-// .catch(err => console.log(err));
-
-
-
-
-
 
 
